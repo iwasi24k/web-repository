ccf48a48a6127efd1bd837f89405b25a1a47f8df
iwasi24k <iwasi.ais.2472@gmail.com>
2025-12-26 23:43:39 +0900

    タイトルのカラーを調整、ヘッダーのボーダーを削除

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -7,9 +7,7 @@ const Header = () => {
         fixed top-0 left-0 right-0 z-50
         h-24
         bg-linear-to-b from-black/80 via-black/50 to-transparent
-        backdrop-blur-md
-        border-b border-white/10
-        "
+        backdrop-blur-md"
     >
         <div className="flex items-center h-full px-16">
             <TitleLogo className="text-3xl" />
