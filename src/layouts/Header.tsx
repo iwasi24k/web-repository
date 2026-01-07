61116d288183e139f142f11a437a9f2208a3d94b
ix.U <no.access.0x@gmail.com>
2026-01-07 18:42:03 +0900

    いらないコメントを削除

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-// import TitleLogo from "../components/ui/TitleLogo";
 import Navigation from "../components/ui/Navigation";
-// import titleLogoImage from "../assets/titleLogo2.svg"
 import titleLogoImage from "../assets/TitleLogo.png"
 
 const Header = () => {
@@ -22,7 +20,6 @@ const Header = () => {
       <div
         className={`grid grid-cols-[auto_1fr_auto] items-center px-[4vw] ${isOpen ? "h-0" : "h-full"}`}
       >
-        {/* <TitleLogo className="hidden xl:block xl:text-[1.5vw] xl:z-60" /> */}
         <img
           className="hidden xl:block xl:w-[8vw]"
           src={titleLogoImage}
